f0198f5148454ee621c12788f775e173489e5eec
Papun1111 <gohanmohapatra@gmail.com>
2025-05-15 23:10:40 +0530

    reverted back

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,32 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next", "plugin:@typescript-eslint/recommended"),
-  {
-    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-    rules: {
-      // Allow use of `any`
-      "@typescript-eslint/no-explicit-any": "off",
-
-      // Allow unused variables that start with `_`
-      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
-
-      // Allow @ts-ignore comments
-      "@typescript-eslint/ban-ts-comment": [
-        "warn",
-        {
-          "ts-ignore": "allow-with-description",
-          "minimumDescriptionLength": 0
-        }
-      ],
-
-      // Allow JSX with unescaped entities
-      "react/no-unescaped-entities": "off",
-
-      // Allow <img> usage instead of forcing <Image />
-      "@next/next/no-img-element": "off",
-    },
-  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
